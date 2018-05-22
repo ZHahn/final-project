9ad0a1848e3d2fa9691d2063e7b3141baddeac5b
ZHahn <997942192@qq.com>
2018-05-22 22:42:04 +0800

    test_scene1

--- Classes/MyScene.h
+++ Classes/MyScene.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "cocos2d.h"  
+USING_NS_CC;
+class MyScene : public cocos2d::CCLayer
+{
+public:
+	static cocos2d::Scene* createScene();
+	virtual bool init();
+	CREATE_FUNC(MyScene);
+};
